faf1390ab1c209042dafb455876f588c1c8f88eb
xinshoutw <me@xinshou.tw>
2026-03-31 19:51:49 +0800

    feat(Exports): update __init__.py with all M1 types
    - Add Block, Entity, EntityKind, ItemStack to top-level exports
    - Add all new event types to models/__init__.py

--- src/pyflayer/__init__.py
+++ src/pyflayer/__init__.py
@@ -1,6 +1,8 @@
 """pyflayer -- A Python-first Mineflayer SDK."""
 
 from pyflayer.bot import Bot
+from pyflayer.models.block import Block
+from pyflayer.models.entity import Entity, EntityKind
 from pyflayer.models.errors import (
     BridgeError,
     ConnectionError,
@@ -10,13 +12,18 @@ from pyflayer.models.errors import (
     PluginError,
     PyflayerError,
 )
+from pyflayer.models.item import ItemStack
 from pyflayer.models.vec3 import Vec3
 
 __all__ = [
+    "Block",
     "Bot",
     "BridgeError",
     "ConnectionError",
+    "Entity",
+    "EntityKind",
     "InventoryError",
+    "ItemStack",
     "NavigationError",
     "NotSpawnedError",
     "PluginError",
--- src/pyflayer/models/__init__.py
+++ src/pyflayer/models/__init__.py
@@ -1,5 +1,7 @@
 """pyflayer domain models."""
 
+from pyflayer.models.block import Block
+from pyflayer.models.entity import Entity, EntityKind
 from pyflayer.models.errors import (
     BridgeError,
     ConnectionError,
@@ -9,18 +11,42 @@ from pyflayer.models.errors import (
     PluginError,
     PyflayerError,
 )
-from pyflayer.models.events import ChatEvent, SpawnEvent
+from pyflayer.models.events import (
+    BlockBrokenEvent,
+    ChatEvent,
+    CollectCompletedEvent,
+    DeathEvent,
+    GoalFailedEvent,
+    GoalReachedEvent,
+    HealthChangedEvent,
+    KickedEvent,
+    SpawnEvent,
+    WhisperEvent,
+)
+from pyflayer.models.item import ItemStack
 from pyflayer.models.vec3 import Vec3
 
 __all__ = [
+    "Block",
+    "BlockBrokenEvent",
     "BridgeError",
     "ChatEvent",
+    "CollectCompletedEvent",
     "ConnectionError",
+    "DeathEvent",
+    "Entity",
+    "EntityKind",
+    "GoalFailedEvent",
+    "GoalReachedEvent",
+    "HealthChangedEvent",
     "InventoryError",
+    "ItemStack",
+    "KickedEvent",
     "NavigationError",
     "NotSpawnedError",
     "PluginError",
     "PyflayerError",
     "SpawnEvent",
     "Vec3",
+    "WhisperEvent",
 ]
