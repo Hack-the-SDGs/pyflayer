6ce0c41bc5820ad899e6b067e128bee8ad371a36
xinshoutw <me@xinshou.tw>
2026-04-06 16:53:17 +0800

    feat(events): add new event classes to the event module

--- src/minethon/__init__.py
+++ src/minethon/__init__.py
@@ -14,20 +14,40 @@ from minethon.models.errors import (
     MinethonConnectionError,
     MinethonError,
 )
-from minethon.models.events import EndEvent
+from minethon.models.events import (
+    BlockBrokenEvent,
+    ChatEvent,
+    CollectCompletedEvent,
+    DeathEvent,
+    EndEvent,
+    GoalFailedEvent,
+    GoalReachedEvent,
+    HealthChangedEvent,
+    KickedEvent,
+    SpawnEvent,
+    WhisperEvent,
+)
 from minethon.models.item import ItemStack
 from minethon.models.vec3 import Vec3
 from minethon.raw import RawBotHandle
 
 __all__ = [
     "Block",
+    "BlockBrokenEvent",
     "Bot",
     "BridgeError",
+    "ChatEvent",
+    "CollectCompletedEvent",
+    "DeathEvent",
     "EndEvent",
     "Entity",
     "EntityKind",
+    "GoalFailedEvent",
+    "GoalReachedEvent",
+    "HealthChangedEvent",
     "InventoryError",
     "ItemStack",
+    "KickedEvent",
     "NavigationAPI",
     "NavigationError",
     "NotSpawnedError",
@@ -36,5 +56,7 @@ __all__ = [
     "MinethonConnectionError",
     "MinethonError",
     "RawBotHandle",
+    "SpawnEvent",
     "Vec3",
+    "WhisperEvent",
 ]
